06be12fcb461e9f7196c75ba17f015b32131b228
deivid321 <v.deivydas2@gmail.com>
2017-06-11 21:25:10 +0300

    upd

--- src/heads/scripts/process.py
+++ src/heads/scripts/process.py
@@ -27,23 +27,21 @@ def preprocess_img(img):
     io.imsave('work.jpeg', img)
     # roll color axis to axis 0
     # img = np.rollaxis(img, -1)
-
     return img
 
 
 def cnn_model():
-
     model = Sequential()
     model.add(ZeroPadding2D((1, 1), input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false'))
-    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(Convolution2D(32, 3, 3, activation='relu', trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
     model.add(ZeroPadding2D((1, 1), trainable='false'))
-    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(Convolution2D(32, 3, 3, activation='relu',trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
     model.add(MaxPooling2D((2, 2), strides=(2, 2), trainable='false'))
 
     model.add(ZeroPadding2D((1, 1), trainable='false'))
-    #model.add(Convolution2D(128, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
-    #model.add(ZeroPadding2D((1, 1), trainable='false'))
-    model.add(Convolution2D(64, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(Convolution2D(128, 3, 3, activation='relu',trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(ZeroPadding2D((1, 1), trainable='false'))
+    model.add(Convolution2D(64, 3, 3, activation='relu', trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
 
     model.add(Dense(8, activation='relu', trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
     model.add(Dropout(0.5))
@@ -60,9 +58,7 @@ all_img_paths = glob.glob(os.path.join(root_dir, '*.jpeg'))
 np.random.shuffle(all_img_paths)
 for img_path in all_img_paths:
     img = preprocess_img(io.imread(img_path))
-    # label = get_class(img_path)
     imgs.append(img)
-    # labels.append(label)
 
 X = np.array(imgs, dtype='float32')
 # print X
@@ -94,21 +90,20 @@ model.summary()
 
 # Load test dataset
 X_test = []
-y_test = genfromtxt('tarkim_test/yy.csv', delimiter=',')
+Y_test = genfromtxt('tarkim_test/yy.csv', delimiter=',')
 root_dir = 'tarkim_test/images/'
-imgs = []
 
+imgs = []
 all_img_paths = glob.glob(os.path.join(root_dir, '*.jpeg'))
 for img_path in all_img_paths:
-    X_test.append(preprocess_img(io.imread(img_path)))
+    imgs.append(preprocess_img(io.imread(img_path)))
 
-X_test = np.array(X_test)
-y_test = np.array(y_test)
 
-hh = X_test[0]
+X_test = np.array(imgs, dtype='float32')
+Y_test = np.array(Y_test)
+
 # predict and evaluate
-y_pred = model.predict(X_test, batch_size=batch_size)
-acc = np.sum(y_pred == y_test) / np.size(y_pred)
+Y_pred = model.predict(X_test)
+acc = np.sum(Y_pred == Y_test) / np.size(Y_pred)
 print("Test accuracy = {}".format(acc))
-# Make one hot targets
-# Y = np.eye(NUM_CLASSES, dtype='uint8')[labels]
+
