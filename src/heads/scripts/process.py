3c8256b49c6b326be4c6f0296b2ec25b275c763e
deivid321 <v.deivydas2@gmail.com>
2017-06-11 21:10:18 +0300

    Vgg trying

--- src/heads/scripts/process.py
+++ src/heads/scripts/process.py
@@ -6,18 +6,14 @@ import os
 import glob
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import Conv2D, ZeroPadding2D, Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import SGD, RMSprop
-from keras import backend as K
+import keras as keras
 from keras.optimizers import SGD
-from keras.callbacks import LearningRateScheduler, ModelCheckpoint
-import pandas as pd
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-from IPython import embed
+
 NUM_CLASSES = 4
 IMG_SIZE = 400
-
 
 def preprocess_img(img):
     # central square crop
@@ -28,36 +24,33 @@ def preprocess_img(img):
           :]
     # rescale to standard size
     img = transform.resize(img, (IMG_SIZE, IMG_SIZE))
-    # io.imsave('work.jpeg', img)
+    io.imsave('work.jpeg', img)
     # roll color axis to axis 0
-    #img = np.rollaxis(img, -1)
+    # img = np.rollaxis(img, -1)
 
     return img
 
 
 def cnn_model():
+
     model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false'))
+    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(ZeroPadding2D((1, 1), trainable='false'))
+    model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), trainable='false'))
 
-    model.add(Conv2D(16, (3, 3), padding='same', init="normal",
-                     input_shape=(IMG_SIZE, IMG_SIZE, 3),
-                     activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(ZeroPadding2D((1, 1), trainable='false'))
+    #model.add(Convolution2D(128, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    #model.add(ZeroPadding2D((1, 1), trainable='false'))
+    model.add(Convolution2D(64, 3, 3, activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3), trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
 
-    model.add(Conv2D(32, (3, 3), padding='same', init="normal",  # model.add(Conv2D(64, (3, 3), padding='same',
-                     activation='relu'))  # activation='relu'))
-    model.add(Conv2D(32, (3, 3), activation='relu', init="normal"))  # model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))  # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.2))		    #model.add(Dropout(0.2))
-    model.add(Conv2D(64, (3, 3), padding='same', init="normal",  # model.add(Conv2D(128, (3, 3), padding='same',
-                     activation='relu'))  # activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu', init="normal"))  # model.add(Conv2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dense(8, activation='relu', trainable='false', kernel_regularizer=keras.regularizers.l2(0.01)))
+    model.add(Dropout(0.5))
+    model.add(Dense(NUM_CLASSES, activation='linear'))
 
-    model.add(Flatten())
-    model.add(Dense(8, activation='relu', init="normal"))
-    model.add(Dropout(0.25))
-    model.add(Dense(NUM_CLASSES, activation='linear', init="normal"))
     return model
+
 
 root_dir = 'train_data/images/'
 imgs = []
@@ -72,31 +65,31 @@ for img_path in all_img_paths:
     # labels.append(label)
 
 X = np.array(imgs, dtype='float32')
-#print X
+# print X
 Y = genfromtxt('train_data/yy.csv', delimiter=',')
-#print Y
+# print Y
 
 model = cnn_model()
-
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 # let's train the model using RMSprop
 lr = 0.001
 rms = RMSprop(lr=lr, rho=0.9)
-model.compile(loss='mean_squared_error',
-              optimizer=rms,
-              metrics=['accuracy'])
-
+# model.compile(loss='mean_squared_error',
+#              optimizer=rms,
+#              metrics=['accuracy'])
+model.compile(optimizer=sgd, loss='categorical_crossentropy') #for vgg model
 batch_size = 8
 epochs = 4
 
 model.summary()
-#embed()
+# embed()
 
-model.fit(X, Y,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_split=0.2
-          )
+#model.fit(X, Y,
+#        batch_size=batch_size,
+#        epochs=epochs,
+#        verbose=1,
+#        validation_split=0.2
+#       )
 
 
 # Load test dataset
@@ -112,8 +105,9 @@ for img_path in all_img_paths:
 X_test = np.array(X_test)
 y_test = np.array(y_test)
 
+hh = X_test[0]
 # predict and evaluate
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test, batch_size=batch_size)
 acc = np.sum(y_pred == y_test) / np.size(y_pred)
 print("Test accuracy = {}".format(acc))
 # Make one hot targets
